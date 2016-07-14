96365fb78e50b034660b5168cee25cef5ec82446
schmidt-sebastian <mrschmidt@google.com>
2016-07-14 12:22:45 -0700

    Adding ActivityIndicator to Dice Roller
    This patch includes two changes:
    - The dice rolling logic is updated to always produce a different numer on each
      roll, so that users know that they have generated a new number
    - An actitvity indicator is added to indicate syncing
    
    Change-Id: I89a5501152a6ef8dd8dda3f7f4a21a6d545f3a5f

--- Syncbase/Example/Dice Roller/DiceViewController.swift
+++ Syncbase/Example/Dice Roller/DiceViewController.swift
@@ -11,12 +11,15 @@ let rowKey = "result"
 
 class DiceViewController: UIViewController {
   @IBOutlet weak var numberLabel: UILabel!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   var collection: Collection?
+  var currentDieRoll = UInt8(2) // The starting number from the XIB
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     do {
+      activityIndicator.alpha = 0.0
       collection = try Syncbase.database().collection(collectionName)
     } catch let e {
       print("Unexpected error: \(e)")
@@ -57,8 +60,12 @@ class DiceViewController: UIViewController {
     if let value = lastValue {
       // Get the single byte out via typecasting to an array of UInt8. This will be unnecessary
       // when we have VOM support in Swift.
-      let num = unsafeBitCast(value.bytes, UnsafePointer<UInt8>.self).memory
-      numberLabel.text = num.description
+      currentDieRoll = unsafeBitCast(value.bytes, UnsafePointer<UInt8>.self).memory
+      numberLabel.text = currentDieRoll.description
+
+      UIView.animateWithDuration(0.35) {
+        self.activityIndicator.alpha = 0.0
+      }
     }
   }
 
@@ -68,7 +75,16 @@ class DiceViewController: UIViewController {
   }
 
   @IBAction func didPressRollDie(sender: UIButton) {
-    var nextNum = UInt8(arc4random_uniform(6) + 1)
+    var nextNum: UInt8
+
+    UIView.animateWithDuration(0.35) {
+      self.activityIndicator.alpha = 1.0
+    }
+
+    repeat {
+      nextNum = UInt8(arc4random_uniform(6) + 1)
+    } while (nextNum == currentDieRoll)
+
     // Right now we can only store NSData, so we have to serialize this number to store it.
     // Soon we will have VOM support and can just put the raw int as the value and expect
     // it to work properly.
